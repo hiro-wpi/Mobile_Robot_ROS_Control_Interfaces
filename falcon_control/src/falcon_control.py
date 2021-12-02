2e98c4d16c61f4d962401c28246004249c512c22
malpurepiyush07@gmail.com <malpurepiyush07@gmail.com>
2021-12-02 15:39:21 -0500

    Falcon changes

--- falcon_control/src/falcon_control.py
+++ falcon_control/src/falcon_control.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python
 
+# Task space of falcon limits ::
+# Z limits : 0.074 (Inside) to 0.176 (Outside)
+# Y limits : -0.056 (Down) to 0.062 (Up)
+# X limits : -0.056 (Left) to 0.062 (Right)
+
 import rospy
 from geometry_msgs.msg import Point, Twist, Vector3
+import time
+from sensor_msgs.msg import LaserScan
 
+COLLISION_CLEARANCE = 1
+    
 
 class FalconControl():
-
     def __init__(self):
         # ROS
         # subscribe to falcon
         self.falcon_sub = rospy.Subscriber("falcon_pos", Point, self.falcon_callback, queue_size=1)
+        self.laser_subscriber = rospy.Subscriber("base_scan", LaserScan, self.get_feedback)
+        
         # publish the command
         self.command_pub = rospy.Publisher("base_controller/cmd_vel", Twist, queue_size=1)
         
         self.feedback_pub = rospy.Publisher("set_falcon_force", Vector3, queue_size=1)
+        self.feedback_flag = 0
+        self.laser_range = []
+
+    def get_feedback(self, data):
+        self.laser_range = data.ranges
+        temp = self.laser_range[160:180]
+        print(temp)
+        # Center laser data start index : 82 and end index :110
+        for i in temp:
+            if i <= COLLISION_CLEARANCE:
+                self.feedback_flag = 1
+            elif i <= COLLISION_CLEARANCE+1 and i> COLLISION_CLEARANCE:
+                self.feedback_flag = 2
+            else:
+                self.feedback_flag = 0   
 
     def falcon_callback(self, data):
         # If falcon is at home position
@@ -27,18 +52,70 @@ class FalconControl():
             wz = -fx
             if vx < 0:
                 wz = -wz # flip direction when going backwards
-
+            
         # Publsish
         cmd_vel = Twist()
         cmd_vel.linear.x = vx
         cmd_vel.angular.z = wz
         self.command_pub.publish(cmd_vel)
         
+        print("Feedback flag : ",self.feedback_flag)
         feedback_mode = Vector3()
-        feedback_mode.x = 1.5
-        feedback_mode.y = 1.5
-        feedback_mode.z = 1.5
+        if self.feedback_flag == 1:
+            feedback_mode.x = 0.0
+            feedback_mode.y = 0.0
+            feedback_mode.z = 3.5
+            
+        elif self.feedback_flag == 2:
+            feedback_mode.x = 0.0
+            feedback_mode.y = 0.0
+            feedback_mode.z = 1.5
+            #self.feedback_pub.publish(feedback_mode)
+        elif self.feedback_flag == 0:
+            feedback_mode.x = 0.0
+            feedback_mode.y = 0.0
+            feedback_mode.z = 0.0
+        
+        
         self.feedback_pub.publish(feedback_mode)
+        
+        #falcon_x, falcon_y, falcon_z = self.check_home_pos(data.x, data.y, data.z) 
+        # feedback_mode.x = 0.0
+        # feedback_mode.y = 0.0
+        # feedback_mode.z = 0.0
+        # self.feedback_pub.publish(feedback_mode)
+        # print("x : {}, y : {}, z : {}".format(feedback_mode.x, feedback_mode.y, feedback_mode.z))
+        # time.sleep(3)
+
+        # feedback_mode.x = 0.0
+        # feedback_mode.y = -1.0
+        # feedback_mode.z = 0.0
+        # self.feedback_pub.publish(feedback_mode)
+        # print("x : {}, y : {}, z : {}".format(feedback_mode.x, feedback_mode.y, feedback_mode.z))
+        # time.sleep(3)
+
+        # feedback_mode.x = 0.0
+        # feedback_mode.y = -2.0
+        # feedback_mode.z = 0.0
+        # self.feedback_pub.publish(feedback_mode)
+        # print("x : {}, y : {}, z : {}".format(feedback_mode.x, feedback_mode.y, feedback_mode.z))
+        # time.sleep(3)
+
+        # feedback_mode.x = 0.0
+        # feedback_mode.y = -4.0
+        # feedback_mode.z = 0.0
+        # self.feedback_pub.publish(feedback_mode)
+        # print("x : {}, y : {}, z : {}".format(feedback_mode.x, feedback_mode.y, feedback_mode.z))
+        # time.sleep(3)
+
+        # feedback_mode.x = 0.0
+        # feedback_mode.y = -8.0
+        # feedback_mode.z = 0.0
+        # self.feedback_pub.publish(feedback_mode)
+        # print("x : {}, y : {}, z : {}".format(feedback_mode.x, feedback_mode.y, feedback_mode.z))
+        # time.sleep(3)
+
+
 
     def check_home_pos(self, x=0, y=0, z=0.125):
         # Check if falcon is around home position
